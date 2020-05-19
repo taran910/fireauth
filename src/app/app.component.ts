33f651160b15170fa028df0685e85a59b62d5cc8
taran910 <tarandeep910@gmail.com>
2020-05-19 17:02:43 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fireauth';
+  constructor(private afAuth: AngularFireAuth){
+  }
+  googleSignInViaPopup(){
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then((userCredentials)=> console.log(userCredentials));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LogComponent } from './log/log.component';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+const config = {
+  apiKey: "AIzaSyBkXwJKOkqwxZro3xkA2_wYKTXwwSguyx4",
+  authDomain: "login-8b30e.firebaseapp.com",
+  databaseURL: "https://login-8b30e.firebaseio.com",
+  projectId: "login-8b30e",
+  storageBucket: "login-8b30e.appspot.com",
+  messagingSenderId: "600998627532",
+  appId: "1:600998627532:web:1e6cd49300862868cd1a69",
+  measurementId: "G-KMXLCS22E2"
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule 
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
